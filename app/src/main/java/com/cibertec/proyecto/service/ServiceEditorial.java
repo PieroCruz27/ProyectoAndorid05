fceeb724921716718e5203953a0dac785d4f3863
JuanRamosAnto <juanramosanto@gmail.com>
2023-09-23 23:37:01 -0500

    Registro Editorial

--- app/src/main/java/com/cibertec/proyecto/service/ServiceEditorial.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceEditorial.java
@@ -1,6 +1,14 @@
 package com.cibertec.proyecto.service;
 
+import com.cibertec.proyecto.entity.Editorial;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ServiceEditorial {
+    @POST("editorial")
+    public Call<Editorial> insertaEditorial(@Body Editorial objEditorial);
 
 
 }
