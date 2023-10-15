d5c18ae1bc525e154c09530e448b66942c5af142
PieroCruz27 <pierobpc7@gmail.com>
2023-10-15 16:17:16 -0500

    Solucion Consulta Por nombre alumno

--- app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
@@ -2,11 +2,23 @@ package com.cibertec.proyecto.service;
 
 import com.cibertec.proyecto.entity.Alumno;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ServiceAlumno {
     @POST("alumno")
     public abstract Call<Alumno> insertaAlumno(@Body Alumno objAlumno);
+
+
+    @GET("alumno/porNombre/{nombre}")
+    public Call<List<Alumno>> listaAlumnosPorNombre(@Path("nombre")String nombre);
+
+
+
+
 }
