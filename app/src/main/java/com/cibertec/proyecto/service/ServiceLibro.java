c4167647875781ae4b3b7b40cdd5900f0636ed1d
Celso-Rodriguez <celso.19rod@gmail.com>
2023-10-21 23:13:44 -0500

    Examen Libro Consulta

--- app/src/main/java/com/cibertec/proyecto/service/ServiceLibro.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceLibro.java
@@ -2,9 +2,13 @@ package com.cibertec.proyecto.service;
 
 import com.cibertec.proyecto.entity.Libro;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ServiceLibro {
 
@@ -12,4 +16,6 @@ public interface ServiceLibro {
     public abstract Call<Libro> insertaLibro(@Body Libro objLibro);
 
 
+    @GET("libro/porTitulo/{titulo}")
+    public Call<List<Libro>> listaLibro (@Path("titulo") String titulo);
 }
