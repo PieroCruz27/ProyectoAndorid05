44d89f796208a3a8deef74792165e57846a73d4f
PieroCruz27 <pierobpc7@gmail.com>
2023-11-08 17:30:41 -0500

    Avance de crud Alumno

--- app/src/main/java/com/cibertec/proyecto/vista/crud/AlumnoCrudFormularioActivity.java
+++ app/src/main/java/com/cibertec/proyecto/vista/crud/AlumnoCrudFormularioActivity.java
@@ -1,16 +1,34 @@
 package com.cibertec.proyecto.vista.crud;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cibertec.proyecto.R;
 import com.cibertec.proyecto.util.NewAppCompatActivity;
 
 public class AlumnoCrudFormularioActivity extends NewAppCompatActivity {
 
+
+    Button btnRegresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno_crud_formulario);
+
+        btnRegresar = findViewById(R.id.btnCrudAlumnoRegresar);
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        AlumnoCrudFormularioActivity.this,
+                        AlumnoCrudListaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/cibertec/proyecto/vista/crud/AlumnoCrudListaActivity.java
+++ app/src/main/java/com/cibertec/proyecto/vista/crud/AlumnoCrudListaActivity.java
@@ -1,6 +1,10 @@
 package com.cibertec.proyecto.vista.crud;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.cibertec.proyecto.R;
 import com.cibertec.proyecto.util.NewAppCompatActivity;
@@ -9,11 +13,29 @@ import com.cibertec.proyecto.util.NewAppCompatActivity;
 public class AlumnoCrudListaActivity extends NewAppCompatActivity {
 
 
+    Button btnLista;
+    Button btnRegistra;
+    ListView lstLista;
    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno_crud_lista);
+
+        btnLista = findViewById(R.id.idCrudAlumnoLista);
+        btnRegistra = findViewById(R.id.idCrudAlumnoRegistrar);
+        lstLista = findViewById(R.id.idCrudAlumnoListView);
+
+        btnRegistra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        AlumnoCrudListaActivity.this,
+                        AlumnoCrudFormularioActivity.class);
+                intent.putExtra("var_tipos", "REGISTRAR");
+                startActivity(intent);
+            }
+        });
     }
 
 
